d313a4c1b0b1667382661d4814ebd729437501e3
Bhoomika R <bhoomiraghunath2004@gmail.com>
2026-02-24 21:14:05 +0530

    initial upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.write("Hello")
+
+console.log("Hello")
+
+var x = 5;
+if(x > 3){
+    console.log("Hi")
+}
+
+var age = 45;
+if(age >= 60){
+    console.log("Senior Citizen")
+}
+else if(age >= 20){
+    console.log("Adult")
+}
+else{
+    console.log("Child")
+}
+
+while(x>0){
+    console.log(x)
+    x = x - 1
+}
+
